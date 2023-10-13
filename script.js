e926951ae83884faf508c6696968200323947629
Janek Zanger <janekzanger@MacBook-Pro-von-Janek.local>
2023-10-13 15:21:57 +0200

    Anmelde-/Registrierseite für Schüler hinzugefügt

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-/*Datenschutzseite*/
+/*Anmeldeseite*/
+/*document.getElementById('showForm').addEventListener('click', function() {
+    document.getElementById('formContainer').classList.remove('hidden');
+});*/
 
+document.addEventListener('DOMContentLoaded', function() {
+    function toggleForm() {
+      var formContainer = document.getElementById('formContainer');
+      formContainer.classList.toggle('hidden');
+    }
+  
+    var showFormButton = document.getElementById('showForm');
+    showFormButton.addEventListener('click', toggleForm);
+  });
+  
 
