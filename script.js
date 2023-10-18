e9bd799ff4e7fe562e156edc230741b70a3faa10
Janek Zanger <janekzanger@MacBook-Pro-von-Janek.local>
2023-10-18 16:29:17 +0200

    Anmelde/Registrierseite updated

--- script.js
+++ script.js
@@ -12,5 +12,17 @@
     var showFormButton = document.getElementById('showForm');
     showFormButton.addEventListener('click', toggleForm);
   });*/
+
+
+  function showText(buttonId) {
+    var textToShow = document.getElementById(buttonId + "Text");
+    var allTexts = document.getElementsByClassName("hidden-text");
+
+    for (var i = 0; i < allTexts.length; i++) {
+      allTexts[i].style.display = "none";
+    }
+
+    textToShow.style.display = "block";
+  }
   
 
