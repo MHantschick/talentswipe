ab162cb8e188c3860b062e1695ecce428fe46f1e
Friedrich B <friedrich@MacBook-Pro-von-Friedrich.local>
2023-10-19 17:20:51 +0200

    startseite

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+/**
+ * startseite slider
+ */
+let slideIndex = 0;
+
+function showSlides() {
+  const slides = document.querySelectorAll(".mySlides");
+  const texts = document.querySelectorAll(".text");
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    texts[i].style.display = "none";
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+
+  setTimeout(function () {
+    texts[slideIndex - 1].style.display = "block";
+  }, 1000);
+
+  setTimeout(showSlides, 7000);
+}
+
+showSlides();
+
 /*Anmeldeseite*/
 /*document.getElementById('showForm').addEventListener('click', function() {
     document.getElementById('formContainer').classList.remove('hidden');
@@ -13,16 +43,13 @@
     showFormButton.addEventListener('click', toggleForm);
   });*/
 
+function showText(buttonId) {
+  var textToShow = document.getElementById(buttonId + "Text");
+  var allTexts = document.getElementsByClassName("hidden-text");
 
-  function showText(buttonId) {
-    var textToShow = document.getElementById(buttonId + "Text");
-    var allTexts = document.getElementsByClassName("hidden-text");
-
-    for (var i = 0; i < allTexts.length; i++) {
-      allTexts[i].style.display = "none";
-    }
-
-    textToShow.style.display = "block";
+  for (var i = 0; i < allTexts.length; i++) {
+    allTexts[i].style.display = "none";
   }
-  
 
+  textToShow.style.display = "block";
+}
